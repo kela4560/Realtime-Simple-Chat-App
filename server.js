c80ab38bd69a7ab6a18096ce895cd3a348cb5651
kela4560 <55550467+kela4560@users.noreply.github.com>
2019-12-16 11:03:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const io = require('socket.io')(3000)
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 const users = {}
 
+const rooms = {}
+
+app.get('/', (req, res) => {
+  res.render('index', ({ rooms: rooms })
+}
+server.listen(3000)
+  
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
